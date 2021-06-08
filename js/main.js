0a737e5b94bbadff00013ddd9cacab90b6994d49
oleg_nexxxt <nexxxt.07@gmail.com>
2021-06-08 18:50:08 +0300

    08.06 3

--- js/main.js
+++ js/main.js
@@ -155,3 +155,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+function getFileName () {
+    var file = document.getElementById ('uploaded-file').value;
+    file = file.replace (/\\/g, '/').split ('/').pop ();
+    document.getElementById ('file-name').innerHTML = 'Имя файла: ' + file;
+}
